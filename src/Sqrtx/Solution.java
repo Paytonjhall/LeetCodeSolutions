fe928a3787e4e705b8656ea9cd12d4501dd2297c
Payton <paytonjhall@gmail.com>
2023-02-14 15:26:30 -0700

    did some leet code problems

---
+++ src/Sqrtx/Solution.java
@@ -0,0 +1,22 @@
+package Sqrtx;
+
+public class Solution {
+  public static void main(String[] args) {
+    System.out.println(mySqrt(147));
+  }
+
+  public static int mySqrt(int x) {
+    if(x == 0) {
+      return 0;
+    } else if (x < 4) {
+      return 1;
+    }
+    int value = 2;
+    while (true) {
+      if(value * value > x) {
+        return value-1;
+      }
+      value ++;
+      }
+    }
+}
